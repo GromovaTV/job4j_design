a9dcb7de26cb105c2dafef8962e8ea5091904fb6
Tatyana <quininthenorth@gmail.com>
2023-04-27 18:40:35 +0400

    Переписала метод poll()

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,21 +3,21 @@ package ru.job4j.collection;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    int size = 0;
+    int inSize = 0;
+    int outSize = 0;
     public T poll() {
-        T returned;
-        for (int i = 0; i < size; i++) {
-            out.push(in.pop());
+        if (outSize == 0) {
+            while (inSize > 0) {
+                out.push(in.pop());
+                inSize--;
+                outSize++;
+            }
         }
-        returned = out.pop();
-        size--;
-        for (int i = 0; i < size; i++) {
-            in.push(out.pop());
-        }
-        return returned;
+        outSize--;
+        return out.pop();
     }
     public void push(T value) {
         in.push(value);
-        size++;
+        inSize++;
     }
 }
