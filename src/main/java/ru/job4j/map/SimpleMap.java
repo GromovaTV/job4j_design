ff1eebeda970c492225689af9a4575d9b54a4f3d
Tatyana <quininthenorth@gmail.com>
2023-05-01 23:16:32 +0400

    Реализовать собственную структуру данных - HashMap

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,111 @@
+package ru.job4j.map;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+    private static final float LOAD_FACTOR = 0.75f;
+    private int capacity = 8;
+    private int count = 0;
+    private int modCount = 0;
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public boolean put(K key, V value) {
+        if (count >= LOAD_FACTOR * capacity) {
+            expand();
+        }
+        if (table[indexFor(hash(Objects.hash(key)))] == null) {
+            count++;
+        }
+        table[indexFor(hash(Objects.hash(key)))] = new MapEntry(key, value);
+        modCount++;
+        return true;
+    }
+
+    private int hash(int hashCode) {
+        return (hashCode == 0) ? 0 : hashCode % capacity;
+    }
+
+    private int indexFor(int hash) {
+        return hash & (capacity - 1);
+    }
+
+    private void expand() {
+        MapEntry<K, V>[] tmpTable = new MapEntry[capacity];
+        System.arraycopy(table, 0, tmpTable, 0, capacity);
+        capacity = capacity * 2;
+        table = new MapEntry[capacity];
+        count = 0;
+        for (MapEntry<K, V> cell : tmpTable) {
+            if (cell != null) {
+                put(cell.key, cell.value);
+            }
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        MapEntry<K, V> cell = table[indexFor(hash(Objects.hash(key)))];
+        if (cell == null) {
+            return null;
+        }
+        return cell.value;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        MapEntry<K, V> cell = table[indexFor(hash(Objects.hash(key)))];
+        if (cell == null || cell.key == null) {
+            return false;
+        }
+        cell.value = null;
+        cell.key = null;
+        count--;
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<K>() {
+            private int index = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < count;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table[index++].key;
+            }
+        };
+    }
+
+    private static class MapEntry<K, V> {
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleMapTest {
+    SimpleMap<Integer, Integer> simpleMap;
+
+    @Before
+    public void initData() {
+        simpleMap = new SimpleMap<>();
+        simpleMap.put(1, 1);
+        simpleMap.put(2, 2);
+        simpleMap.put(3, 3);
+        simpleMap.put(4, 4);
+        simpleMap.put(5, 5);
+        simpleMap.put(6, 6);
+
+    }
+
+    @Test
+    public void put() {
+        simpleMap.put(1, 2);
+        Assert.assertEquals(Integer.valueOf(2), simpleMap.get(1));
+        Assert.assertEquals(Integer.valueOf(2), simpleMap.get(2));
+    }
+
+
+    @Test
+    public void remove() {
+        simpleMap.remove(1);
+        Assert.assertNull(simpleMap.get(1));
+        Assert.assertFalse(simpleMap.remove(1));
+        Assert.assertFalse(simpleMap.remove(7));
+    }
+
+    @Test
+    public void putWhenExpand() {
+        simpleMap.put(7, 7);
+        Assert.assertEquals(16, simpleMap.getCapacity());
+    }
+}
