b11fddd886a22a400a01baf3b52ab1787227d400
Tatyana <quininthenorth@gmail.com>
2023-05-04 20:47:23 +0400

    Читаем файл конфигурации

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr"));
+        //assertThat(config.value("surname"), is(Matchers.nullValue()));
+        assertThat(config.value("surname"), is("Petrov"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void whenEmptyValue() {
+        String path = "./data/pair_with_empty_values.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
