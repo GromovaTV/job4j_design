97ff164042f4d8dfcdae7970e248867e94819d17
Tatyana <quininthenorth@gmail.com>
2023-05-05 15:41:12 +0400

    Тестирование IO

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -13,7 +13,7 @@ public class Analizy {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 String[] str = line.split(" ");
-                if (str[0].equals("400") || str[0].equals("500")) {
+                if (begin.equals("") && (str[0].equals("400") || str[0].equals("500"))) {
                     begin = str[1];
                 }
                 if (!begin.equals("") && (str[0].equals("200") || str[0].equals("300"))) {
@@ -25,19 +25,16 @@ public class Analizy {
                     end = "";
                 }
             }
+            if (!begin.equals("") && end.equals("")) {
+                list.add(begin + ";");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             list.stream().forEach(out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        Analizy analizy = new Analizy();
-        analizy.unavailable("./res/source.log", "./res/target.csv");
     }
 }
