4efc021339d65598e082b1debf44d358e841872d
Tatyana <quininthenorth@gmail.com>
2023-05-07 13:35:08 +0400

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,13 +10,26 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".class")).forEach(System.out::println);
+        Path start = Paths.get(args[0]);
+        validation(args, start);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validation(String[] args, Path start) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Insufficient number of input parameters. Usage java -jar dir.jar ROOT_FOLDER SEARCH_PARAMETER.");
+        }
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toFile().getAbsoluteFile()));
+        }
+        if (!start.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toFile().getAbsoluteFile()));
+        }
     }
 }
