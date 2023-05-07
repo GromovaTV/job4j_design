a08067a24f8ac31205114b957571ecefa921cc9c
Tatyana <quininthenorth@gmail.com>
2023-05-07 14:24:11 +0400

    Именованные аргументы

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException();
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String arg : args) {
+            var split = arg.split("-", 2);
+            var keyValue = split[1].split("=", 2);
+            var key = keyValue[0];
+            var value = keyValue[1];
+            if (key.isEmpty() || value.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            values.put(key, value);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
