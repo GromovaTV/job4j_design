dadf87774534b07eda86b4727d2446bcd47cca7d
Tatyana <quininthenorth@gmail.com>
2023-05-10 13:10:55 +0400

    Slf4j - вывод переменных

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -11,5 +11,18 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        String name = "Petr";
+        int age = 30;
+        boolean isWorking = true;
+        byte b = 8;
+        char symbol = 102;
+        double d = 1.5d;
+        float fl = 30.6f;
+        long l = 10000L;
+        short s = 12;
+
+        LOG.debug("User info name : {}, age : {}, isWorking : {}, b : {}, symbol : {}, d : {}, fl : {}, l : {}, s : {}",
+                name, age, isWorking, b, symbol, d, fl, l, s);
+
     }
 }
