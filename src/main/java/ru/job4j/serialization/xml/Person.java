9652fb263adecb03812d980ce97c50c59ccdcbb1
Tatyana <quininthenorth@gmail.com>
2023-05-10 19:22:08 +0400

    JAXB. Преобразование XML в POJO

--- src/main/java/ru/job4j/serialization/xml/Person.java
+++ src/main/java/ru/job4j/serialization/xml/Person.java
@@ -1,11 +1,32 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
+
+    @XmlAttribute
     private String name;
+
+    @XmlAttribute
     private int age;
+
+    @XmlAttribute
     private boolean gender;
     private Contact contact;
+
+    @XmlElementWrapper(name = "kids")
+    @XmlElement(name = "kid")
+    private String[] statuses;
     private String[] kids;
+
+    public Person() {
+    }
 
     public Person(String name, int age, boolean gender, Contact contact, String[] kids) {
         this.name = name;
@@ -13,5 +34,19 @@ public class Person {
         this.gender = gender;
         this.contact = contact;
         this.kids = kids;
+    }
+
+    public static void main(String[] args) throws JAXBException {
+        final Person person = new Person("Kate", 30, true,
+                new Contact("11-111"), new String[] {"Tom", "Jane"});
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+        }
     }
 }
