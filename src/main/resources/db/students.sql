fce44121e5b5e73a57cb478c53c0a88ec405d738
Tatyana <quininthenorth@gmail.com>
2023-05-16 18:19:20 +0400

    Выравнивание текста

--- src/main/resources/db/students.sql
+++ src/main/resources/db/students.sql
@@ -5,6 +5,7 @@ create table students(
 	year integer,
 	score numeric
 );
+
 insert into students (name, faculty, year, score)
 values ('Maria', 'Economy and finance', 2, 4.5);
 update students set name = 'Maria Petrova';
