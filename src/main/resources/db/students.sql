7122425855ffedeb7278c86870ea2e02b0035af0
Tatyana <quininthenorth@gmail.com>
2023-05-16 15:46:13 +0400

    SQL: Create, Update, Insert

---
+++ src/main/resources/db/students.sql
@@ -0,0 +1,11 @@
+create table students(
+	id serial primary key,
+	name varchar(255),
+	faculty text,
+	year integer,
+	score numeric
+);
+insert into students (name, faculty, year, score) values 
+('Maria', 'Economy and finance', 2, 4.5);
+update students set name = 'Maria Petrova';
+delete from students;
