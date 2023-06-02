4a47c45b312bb49d301a060c2d4dee8c08540f1c
Tatyana <quininthenorth@gmail.com>
2023-06-02 16:40:09 +0400

    Реализация кеша на SoftReference

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,25 @@
+package ru.job4j.cache;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DirFileCache extends AbstractCache<String, String> {
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        System.out.println("Загрузка файла в кеш");
+        try {
+            String content = Files.readString(Paths.get(String.format("%s/%s",
+                        this.cachingDir, key)));
+            return content;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -0,0 +1,46 @@
+package ru.job4j.cache;
+
+import java.util.Scanner;
+
+public class Emulator {
+    private AbstractCache dc;
+
+    public void setDC(String name) {
+        String path = String.format("C:\\projects\\job4j_design\\res/%s", name);
+        this.dc =  new DirFileCache(path);
+    }
+
+    public void init() {
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            System.out.println("Выберите действие:\n 1. Указать директорию\n "
+                    + "2. Загрузить файл\n 3. Получить файл\n 4. Выход");
+            String input = in.nextLine();
+            if (input.equals("1")) {
+                System.out.println("Введите название директории");
+                String name = in.nextLine();
+                setDC(name);
+
+            }
+            if (input.equals("2")) {
+                System.out.println("Введите имя файла для загрузки");
+                String fileName = in.nextLine();
+                dc.load(fileName);
+            }
+            if (input.equals("3")) {
+                System.out.println("Введите имя файла");
+                String fileName = in.nextLine();
+                System.out.println(dc.get(fileName));
+            }
+            if (input.equals("4")) {
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Emulator em = new Emulator();
+        em.init();
+    }
+}
